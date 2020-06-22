c7fdd15bcd6d772c53f8fdea29155b366fe7cb14
Sandesh <kandel.sandesh@outlook.com>
2020-06-22 13:23:25 +1000

    worked on search

--- listings/views.py
+++ listings/views.py
@@ -2,6 +2,8 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
+from .choices import price_choices, bedroom_choices,state_choices
+
 
 def index(request):
     listings= Listing.objects.order_by('-list_date').filter(is_published=True)
@@ -11,6 +13,7 @@ def index(request):
 
     context={
         'listings': paged_listings
+        
     }
     return render(request,'listings/listings.html', context)
 
@@ -24,4 +27,26 @@ def listing(request, listing_id):
     return render(request,'listings/listing.html', context)
 
 def search(request):
-    return render(request,'listings/search.html')
+    queryset_list=Listing.objects.order_by('-list_date')
+
+    #keywords
+    if 'keywords' in request.GET:
+        keywords=request.GET['keywords']
+        if keywords:
+            queryset_list: queryset_list.filter(description__icontains=keywords)
+
+    #City
+    if 'city' in request.GET:
+        city=request.GET['city']
+    if city:
+        queryset_list: queryset_list.filter(city__iexact=city)
+
+    context={
+        'state_choices':state_choices,
+        'bedroom_choices':bedroom_choices,
+        'price_choices':price_choices,
+        'listings':queryset_list,
+        'values':request.GET
+
+    }
+    return render(request,'listings/search.html',context)
